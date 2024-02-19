cc39f945ed4d2159b5b6d7291faece46a6d4efba
Issasheep <jordantait4411@gmail.com>
2024-02-20 07:47:17 +1000

    basic addtions to the "work space"

--- cybersecurity/python projects/python tests/game_functions.py
+++ cybersecurity/python projects/python tests/game_functions.py
@@ -2,20 +2,27 @@ import random
 import pygame
 import sys
 import os
-
 amount_dropped = 10
 
 item_dict = []
 roll_values = []
 
 all_item_types = ["weapon","armor","spells","unique"]
+rareities = ["common","uncommon","rare","legendary"]
+poassble_min = ["200","400","600","800"]
+poassble_max = ["200","400","800","1000"]
 
 def item_list():
     counter = 0
     while amount_dropped >= counter:
+        min_max_stats = random.randrange(0,4)
         item_dict.append(all_item_types[random.randrange(0,4)])
-        item_dict.append(random.randrange(600,1001))
+        item_dict.append(roll_values[random.randrange(poassble_min[min_max_stats],poassble_max[min_max_stats],10)])
         counter +=1
     return item_dict
+
+print (item_list())
+
+
 
 #make a fucntion randomizes numbers from 300-1001 and make a ductonary with the output of the item_list functiuon 
---
+++ python/python_work_space/first_run.py
@@ -0,0 +1,22 @@
+import os
+import webbrowser
+#setting up a basic workspace with all the tab that i would use to work or email people
+
+#if os.path.exists("testing.txt"):
+    #os.remove("testing.txt")
+#else:
+    #print("no such file")
+
+
+
+
+#f = open("testing", "x")
+#print(f)
+
+
+webbrowser.open_new('https://www.google.com/gmail/about/')
+webbrowser.open_new('https://www.w3schools.com/python/default.asp')
+webbrowser.open_new('https://github.com/')
+#quit(0)
+#webbrowser.open_new('')
+#webbrowser.open_new('')
