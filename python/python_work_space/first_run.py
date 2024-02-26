b77c4daef90cfb3d2d8fdbbbbbcee71d30f12474
Issasheep <jordantait4411@gmail.com>
2024-02-26 15:39:06 +1000

    26/02/2024 python work

--- python/python_work_space/first_run.py
+++ python/python_work_space/first_run.py
@@ -2,7 +2,8 @@ import os
 import webbrowser
 import requests
 import sys
-import urllib.request
+import urllib.request 
+from urllib.request import urlopen
 #setting up a basic workspace with all the tab that i would use to work or email people
 
 #if os.path.exists("testing.txt"):
@@ -17,10 +18,11 @@ import urllib.request
 #print(f)
 
 
-webbrowser.open_new('https://www.google.com/gmail/about/')
-webbrowser.open_new('https://www.w3schools.com/python/default.asp')
-webbrowser.open_new('https://github.com/')
-os.system("D:\SteamLibrary\steamapps\common\SHENZHEN IO\Shenzhen.exe")
+#webbrowser.open_new('https://www.google.com/gmail/about/')
+#webbrowser.open_new('https://www.w3schools.com/python/default.asp')
+#webbrowser.open_new('https://github.com/')
+os.system("start D:\SteamLibrary\steamapps\common\Batman Arkham Asylum GOTY\Binaries\ShippingPC-BmGame.exe")
+
 #quit(0)
 #webbrowser.open_new('')
 #webbrowser.open_new('')
