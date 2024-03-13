9936632426a393672005145bae0ee053b915ecdc
Issasheep <jordantait4411@gmail.com>
2024-03-13 14:15:49 +1000

    13/03/24 updates

---
+++ cs 50/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <cs50.h>
+
+using namespace std;
+
+int main (){
+    get_int("what's x")
+    cout <<"hello World!"<< endl;
+}
---
+++ python/python_work_space/cs50python_test.py
@@ -0,0 +1,9 @@
+width = int(input("how large? "))
+length = int(input("how long? "))
+counter = 1
+for i in range(width):
+    print ("\n")
+    #counter += -1
+    length -= counter
+    for i in range(length + counter):
+        print (" * ",end="")
