881c344670f37aec4f6faa839c582f89226c3733
Issasheep <jordantait4411@gmail.com>
2024-05-10 12:25:47 +1000

    10/05/24

--- python/python_work_space/main.cpp
+++ python/python_work_space/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
+
+
+void loot_drop_system(){
+    string item_rareity_list[6] {"common","uncommon","rare","epic","legendary","hero's"};
+    /*
+    int possable_number_of_effects[6] {0,1,2,4,6,9};
+    */
+    string possable_item_effects[4] {"fire damage","frost damage","crit chance","crit damage"};
+    for (string i : item_rareity_list){
+              cout << i << "\n";
+}
+}
+
 int main() {
-  for (int i = 0; i < 100; i++ ) 
-  {
-    if
-    cout << i << "\n" ;
-  }
+  loot_drop_system();
   return 0;
 }
 /*
@@ -17,3 +26,17 @@ int all_possable_item_stats(){
     return
 }
 */
+
+  /*
+  int i = 0;
+  for (;i < 101; i++ )
+  {
+    if ((i % 2) == 1) {
+      cout << i << " is odd\n" ;
+    }
+    else {
+      cout << i << " is even\n" ;
+    }
+  }
+  return 0;
+  */
