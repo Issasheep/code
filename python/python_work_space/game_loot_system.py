2cc2482f48d3b7bc74c84beb4ca6e754adb87b00
Issasheep <jordantait4411@gmail.com>
2024-05-10 10:44:10 +1000

    10/05/2024

--- python/python_work_space/game_loot_system.py
+++ python/python_work_space/game_loot_system.py
@@ -3,10 +3,9 @@ import random
 #a item drops 
 #that item has a chance to have some rarity on it
 
-item_rareity_list = ["common","uncommon","rare","epic","legendary","heros"]
+item_rareity_list = ["common","uncommon","rare","epic","legendary","hero's"]
 possable_number_of_effects = [0,random.randint(1,2),random.randint(2,3),random.randint(3,5),random.randint(5,8),random.randint(8,10)]
 possable_item_effects = ["fire damage","frost damage","crit chance","crit damage"]
-item_dropped = [item_rareity_list,possable_number_of_effects,possable_item_effects]
 
 #all this will be assuming a item dropping event happened
 
@@ -14,6 +13,9 @@ item_dropped = [item_rareity_list,possable_number_of_effects,possable_item_effec
 item_dropped_score = random.randint(1,1000)
 
 #if item_dropped_score >= 200: #common
+
+def item_rareity(item_rareity_list,item_dropped_score):
+    
 
 for i in item_dropped:
     print (i)
---
+++ python/python_work_space/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 100; i++ ) 
+  {
+    if
+    cout << i << "\n" ;
+  }
+  return 0;
+}
+/*
+int all_possable_item_stats(){
+    string item_rareity_list[6] {"common","uncommon","rare","epic","legendary","hero's"};
+    int possable_number_of_effects[6] {0,1,2,4,6,9};
+    string possable_item_effects[4] {"fire damage","frost damage","crit chance","crit damage"};
+    return
+}
+*/
